6ead61c4044efa43455815c7c5fbc388d33559fc
otsang3 <ohhtsang@gmail.com>
2021-01-29 18:23:12 +0000

    added useEffect to fetch seasons from Api in EpisodeMain.js and render season list

--- src/components/EpisodeMain.js
+++ src/components/EpisodeMain.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function EpisodeMain() {
 
+    const [ state, setState ] = useState(null);
+
+    useEffect(() => {
+        fetch('http://api.tvmaze.com/shows/526/seasons')
+            .then(res => res.json())
+            .then(data => setState(data))
+    }, []);
+
+    const renderSeasonList = () => {
+        const seasonsArr = [];
+        state.forEach((season, index) => {
+            seasonsArr.push(
+                <Link key={index} to="/">Season {season.number}</Link>
+            )
+        })
+        return seasonsArr;
+    }
+
+    if (state === null) {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
     return(
         <div>
-            This is the Episode home.
+            {renderSeasonList()}
         </div>
     )
 }
