1a4589379b2d48ffe5cd65c466c4b47cac4fc3cb
otsang3 <ohhtsang@gmail.com>
2021-01-30 15:16:08 +0000

    added banner images and some css styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import mc from './images/digital-mc.png';
+import dmpc from './images/dmpc2.jpeg';
 import EpisodeList from './components/EpisodeList';
 import EpisodeMain from './components/EpisodeMain';
 import HomePage from './components/HomePage';
+import logo from './images/logo.png';
 import NavBar from './components/NavBar';
+
 
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <div className="img-banner">
+          <div className="banner-col">
+            <img className="banner-img1" src={dmpc} alt="dunder-mifflin"/>
+          </div>
+          <div className="banner-col">
+            <img className="banner-img2" src={mc} alt="michaels-cott"/>
+          </div>
+          <div className="banner-col">
+            <img className="banner-img3" src={logo} alt="the-office-logo"/>
+          </div>
+        </div>
+        
         <NavBar/>
         <Switch>
           <Route exact path="/" render={() => <HomePage/>}/>
